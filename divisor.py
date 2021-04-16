d2be450b61c0daf204ab49a30d732d8b22afe205
Grace Todd <toddgr@oregonstate.edu>
2021-04-15 21:57:00 -0700

    create for loop to catch divisors

--- divisor.py
+++ divisor.py
@@ -7,3 +7,11 @@
 # Retrieve user input, typecast variable as integer
 num = input("Select a number: ")
 num = int(num)
+
+divisors = []
+
+for i in range(1, int(num / 2) + 1):
+    if num % i == 0:
+        divisors.append(i)
+
+print("Divisors of ", num, "are: ", divisors)
