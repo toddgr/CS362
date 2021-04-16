53cd19187e274297b5eb5c7bc960ae7163031783
Grace Todd <toddgr@oregonstate.edu>
2021-04-15 21:33:29 -0700

    Calculator app, addition function

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# Grace Todd
+# CS 262
+# Calculator program
+# Write a simple calculator app that adds, subtracts, multiplies, and divides
+
+
+
+# Introduction
+print("--- C A L C U L A T O R     A P P L I C A T I O N ---")
+
+# Create variables that are added together, subtracted from eachother, multiplied and divided
+# Request user input
+
+firstNum = input("Enter first number: ")
+secondNum = input("Enter second number: ")
+
+#Addition function
+sum = firstNum + secondNum
+print(firstNum, " + ", secondNum, " = ", sum)
+
+#Subtraction function
+dif = firstNum - secondNum
+print(firstNum, " - ", secondNum, " = ", )
