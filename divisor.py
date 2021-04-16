eb2c0df7a06235b81dbee2b47e5b085422d949d4
Grace Todd <toddgr@oregonstate.edu>
2021-04-15 21:50:10 -0700

    Create divisor app

---
+++ divisor.py
@@ -0,0 +1,9 @@
+# Grace Todd
+# CS 262
+# Divisor app
+
+# Write a program that lists all the possible divisors for given number
+
+# Retrieve user input, typecast variable as integer
+num = input("Select a number: ")
+num = int(num)
