e6e88e2c92293e05c4c95cb65554b70beac78458
Grace Todd <toddgr@oregonstate.edu>
2021-04-15 21:36:13 -0700

    Subtraction function, division error handling

--- calculator.py
+++ calculator.py
@@ -14,10 +14,18 @@ print("--- C A L C U L A T O R     A P P L I C A T I O N ---")
 firstNum = input("Enter first number: ")
 secondNum = input("Enter second number: ")
 
+#Error handling for division funnction
+while secondNum = 0:
+    print("ERROR: Cannot compute division, zero is not valid divisor.")
+    secondNum = input("Enter new number: ")
+
+
 #Addition function
 sum = firstNum + secondNum
 print(firstNum, " + ", secondNum, " = ", sum)
 
 #Subtraction function
 dif = firstNum - secondNum
-print(firstNum, " - ", secondNum, " = ", )
+print(firstNum, " - ", secondNum, " = ", dif)
+
+#Multiplication function
