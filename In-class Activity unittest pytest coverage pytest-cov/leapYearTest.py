703a8702eb09d0e873fb1edbcdb73e3299fe7d71
Grace Todd <toddgr@oregonstate.edu>
2021-05-31 21:57:39 -0700

    Write leapYear function

--- In-class Activity unittest pytest coverage pytest-cov/leapYearTest.py
+++ In-class Activity unittest pytest coverage pytest-cov/leapYearTest.py
@@ -15,6 +15,17 @@
 
 import unittest
 
+def leapYear(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return 'Yes'
+            else:
+                return 'No'
+        else:
+            return 'Yes'
+    else:
+        return 'No'
 
 
 class Testing(unittest.TestCase):
