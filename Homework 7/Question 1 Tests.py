c9885620cbf872c8e1247c24f89e84705422403d
Grace Todd <toddgr@oregonstate.edu>
2021-05-31 17:52:23 -0700

    Wrote first test, assert first four multiples of three equal to 'Fizz'

--- Homework 7/Question 1 Tests.py
+++ Homework 7/Question 1 Tests.py
@@ -8,3 +8,16 @@
 # multiples of both three and five print "FizzBuzz".
 
 # Testing file
+
+import unittest
+import Q1Code
+
+class TestCase(unittest.TestCase):
+
+    # First test asserts that the first four multiples of three are equal to "Fizz"
+    def test1(self):
+        for i in [3, 6, 9, 12]:
+            self.assertEqual(fizzbuzz(i), "Fizz")
+
+if __name__ == "__main__":
+    unittest.main()
