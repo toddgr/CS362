6c73eff0d1aed02f345c5cfe9d7a9899a67d458b
Grace Todd <toddgr@oregonstate.edu>
2021-05-31 22:00:47 -0700

    Add print functions for unittest outputs

--- In-class Activity unittest pytest coverage pytest-cov/leapYearTest.py
+++ In-class Activity unittest pytest coverage pytest-cov/leapYearTest.py
@@ -16,33 +16,38 @@
 import unittest
 
 def leapYear(year):
+    print('The year is: ', year)
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                return 'Yes'
+                print('YES', year, 'is a leap year\n')
+                return True
             else:
-                return 'No'
+                print('NO', year, 'is NOT a leap year\n')
+                return False
         else:
-            return 'Yes'
+            print('YES', year, 'is a leap year\n')
+            return True
     else:
-        return 'No'
+        print('NO', year, 'is NOT a leap year\n')
+        return False
 
 
 class Testing(unittest.TestCase):
     
     # Assert that the function correctly calculates the given year to be a leap year.
     def test1(self):
-        self.assertEqual(leapYear(2020), 'Yes')
-        self.assertEqual(leapYear(1840), 'Yes')
-        self.assertEqual(leapYear(1696), 'Yes')
-        self.assertEqual(leapYear(1708), 'Yes')
+        self.assertEqual(leapYear(2020), True)
+        self.assertEqual(leapYear(1840), True)
+        self.assertEqual(leapYear(1696), True)
+        self.assertEqual(leapYear(1708), True)
 
     # Assert that the function coorectly calculates the given year to NOT be a leap year.
     def test2(self):
-        self.assertEqual(leapYear(1789), 'No')
-        self.assertEqual(leapYear(1967), 'No')
-        self.assertEqual(leapYear(1673), 'No')
-        self.assertEqual(leapYear(1781), 'No')
+        self.assertEqual(leapYear(1789), False)
+        self.assertEqual(leapYear(1967), False)
+        self.assertEqual(leapYear(1673), False)
+        self.assertEqual(leapYear(1781), False)
 
 if __name__ == '__main__':
     unittest.main()
