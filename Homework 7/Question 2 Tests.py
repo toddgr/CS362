6a5bfb20242234dce6ae9c662c253e006a5031d0
Grace Todd <toddgr@oregonstate.edu>
2021-05-31 18:41:52 -0700

    Wrote first test, assert years evenly divisible by four are counted as leap years by testing random multiples of four years

--- Homework 7/Question 2 Tests.py
+++ Homework 7/Question 2 Tests.py
@@ -3,6 +3,23 @@
 # Spring 2021
 
 # Homework 7
-# Question 2: From homework 1, rewrite the leap year program using the test-first approach. 
+# Question 2: From homework 1, rewrite the leap year program using the test-first approach.
+# Years that are evenly divisible by 4 except years that are evenly divisible by 100 unless
+# the years are also evenly divisible by 400 
 
 # Testing file
+
+import unittest
+import Q2Code
+
+class TestCase(unittest.TestCase):
+
+    # First test asserts that years that ARE evenly divisible by 4 are counted as a leap year by the function.
+    def test1(self):
+        self.assertEqual(leapYear(1888), 'Yes')
+        self.assertEqual(leapYear(1828), 'Yes')
+        self.assertEqual(leapYear(2000), 'Yes')
+        self.assertEqual(leapYear(1944), 'Yes')
+
+if __name__ == '__main__':
+    unittest.main()
