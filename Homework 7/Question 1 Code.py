46386e571231985705ef050f3798fc273de8e554
Grace Todd <toddgr@oregonstate.edu>
2021-05-31 17:01:07 -0700

    Create Question 1 code and testing files

---
+++ Homework 7/Question 1 Code.py
@@ -0,0 +1,10 @@
+# Grace Todd
+# CS 362: Software Engineering II
+# Spring 2021
+
+# Homework 7
+# Question 1: Write a program that prints the numbers from 1 to 100. But for multiples of three print 
+# "Fizz" instead of the number nad for the multiples of five print "Buzz". For numbers which are 
+# multiples of both three and five print "FizzBuzz".
+
+# Code implementation
