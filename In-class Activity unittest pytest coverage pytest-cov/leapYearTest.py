8c285ab824672343c5fd23dbd1c315d5feb94465
Grace Todd <toddgr@oregonstate.edu>
2021-05-31 21:56:50 -0700

    Write unittest cases for leapYear function before implementation

---
+++ In-class Activity unittest pytest coverage pytest-cov/leapYearTest.py
@@ -0,0 +1,37 @@
+# Grace Todd
+# CS 362: Software Engineering II
+# Spring 2021
+
+# In-class activity: unittest, pytest, Coverage, pytest-cov
+
+# Write tests for leap year program before you begin implementation. 
+# Make a commit. 
+# Write code and then run the test - then make a commit
+# Document Output with unittest
+# Run pytest and document the output.
+# Run coverage - with report option and coverage with report -m. Again document the output. 
+# Run coverage with html - document the output. 
+# Run pytest cov - document the output
+
+import unittest
+
+
+
+class Testing(unittest.TestCase):
+    
+    # Assert that the function correctly calculates the given year to be a leap year.
+    def test1(self):
+        self.assertEqual(leapYear(2020), 'Yes')
+        self.assertEqual(leapYear(1840), 'Yes')
+        self.assertEqual(leapYear(1696), 'Yes')
+        self.assertEqual(leapYear(1708), 'Yes')
+
+    # Assert that the function coorectly calculates the given year to NOT be a leap year.
+    def test2(self):
+        self.assertEqual(leapYear(1789), 'No')
+        self.assertEqual(leapYear(1967), 'No')
+        self.assertEqual(leapYear(1673), 'No')
+        self.assertEqual(leapYear(1781), 'No')
+
+if __name__ == '__main__':
+    unittest.main()
