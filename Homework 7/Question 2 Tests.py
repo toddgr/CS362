0e40b8791ac30e6f4af2f3f5c1daa686aee5fc59
Grace Todd <toddgr@oregonstate.edu>
2021-05-31 18:30:39 -0700

    Create files from question 2

---
+++ Homework 7/Question 2 Tests.py
@@ -0,0 +1,8 @@
+# Grace Todd
+# CS 362: Software Engineering II
+# Spring 2021
+
+# Homework 7
+# Question 2: From homework 1, rewrite the leap year program using the test-first approach. 
+
+# Testing file
