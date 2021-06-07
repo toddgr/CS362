4688ea705904da57bb370887c14fbed8b7d69247
Grace Todd <toddgr@oregonstate.edu>
2021-06-06 17:44:18 -0700

    Edit workflow test

--- Extra Credit Assignment/Question1.py
+++ Extra Credit Assignment/Question1.py
@@ -1,21 +1,22 @@
 """
-	Grace Todd
-	CS 362: Software Engineering II
-	Spring 2021
+Grace Todd
+CS 362: Software Engineering II
+Spring 2021
 
-	EXTRA CREDIT ASSIGNMENT QUESTION 1
-	Write a program that reverses a sentence. 
-	Input - ask the user for a sentence: a long string 
-	containing multiple words. Return to the user 
-	the same string,  with the words in backward order.
+EXTRA CREDIT ASSIGNMENT QUESTION 1
+Write a program that reverses a sentence.
+Input - ask the user for a sentence: a long string
+containing multiple words. Return to the user
+the same string,  with the words in backward order.
 """
 
+
 def reverseSentence(str):
-	# Split up the string into sentences, put into a list
+	#Split up the string into sentences, put into a list
 	sentence = str.split()
-	# Reverse the order of the values in the list, reverse sentence by word
+	#Reverse the order of the values in the list, reverse sentence by word
 	revSentence = sentence[:: -1]
-	# Join reversed words together with spaces, so that the output is a string and not a list
+	#Join reversed words together with spaces, so that the output is a string and not a list
 	newSentence = " ".join(revSentence)
-	# Print new sentence
+	#Print new sentence
 	print(newSentence)
